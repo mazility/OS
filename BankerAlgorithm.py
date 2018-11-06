90bdb85ccb07c7e6f852fbe873df6a544467afb9
mazility <pannachet.l@gmail.com>
2018-11-06 15:48:19 +0700

    Update Banker's Algorithm

--- BankerAlgorithm.py
+++ BankerAlgorithm.py
@@ -1,29 +1,29 @@
 import numpy as np
 
-n_processes = int(input('Number of processes > '))
-n_resources = int(input('Number of resources > '))
+number_processes = int(input('Number of processes: '))
+number_resources = int(input('Number of resources: '))
 
-available_resources = [int(x) for x in input('Claim vector? ').split(' ')]
+available_resources = [int(x) for x in input('Total system resources, with a space: ').split(' ')]
 
-currently_allocated = np.array(
-    [[int(x) for x in input('Currently allocated for process ' + str(i + 1) + '? ').split(' ')] for i in
-     range(n_processes)])
-max_demand = np.array([[int(x) for x in input('Maximum demand from process ' + str(i + 1) + '? ').split(' ')] for i in
-                       range(n_processes)])
+currently_allocated_resources = np.array(
+    [[int(x) for x in input('Currently allocated resources ' + str(i + 1) + ' ? with a space:').split(' ')]
+    for i in range(number_processes)])
+maximum_resources = np.array([[int(x) for x in input('Maximum resources ' + str(i + 1) + ' ? with a space:').split(' ')] for i in
+                       range(number_processes)])
 
-total_available = available_resources - np.sum(currently_allocated, axis=0)
+total_available_resources = available_resources - np.sum(currently_allocated_resources, axis=0)
 
-running = np.ones(n_processes)
+running = np.ones(number_processes)
 
 while np.count_nonzero(running) > 0:
     at_least_one_allocated = False
-    for p in range(n_processes):
+    for p in range(number_processes):
         if running[p]:
-            if all(i >= 0 for i in total_available - (max_demand[p] - currently_allocated[p])):
+            if all(i >= 0 for i in total_available_resources - (maximum_resources[p] - currently_allocated_resources[p])):
                 at_least_one_allocated = True
                 print(str(p) + ' is running')
                 running[p] = 0
-                total_available += currently_allocated[p]
+                total_available_resources += currently_allocated_resources[p]
     if not at_least_one_allocated:
         print('Status: Unsafe')
         exit()
